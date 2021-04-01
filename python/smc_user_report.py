397db59711e296337294dbdbe58d835efde4a42a
jg3 <jg3@topsecret.net>
2021-04-01 12:30:57 -0400

    screen output is nice, tabs and commas into .csv open badly in MsXl

--- python/smc_user_report.py
+++ python/smc_user_report.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-This script will get all tags (host groups) in Stealthwatch using the REST API.
+This script will get all users and groups in Stealthwatch using the REST API.
 
 For more information on this API, please visit:
 https://developer.cisco.com/docs/stealthwatch/
@@ -47,16 +47,17 @@ QUIET = 0
 
 # Enter all authentication info
 SMC_USER = "admin"
-SMC_PASSWORD = ""
+SMC_PASSWORD = "C1sco12345"
 SMC_HOST = "localhost"
 SMC_TENANT_ID = "123"
 # For Tenant ID on your system run: 'ls /lancope/var/smc/config/domain_*'
 
-# when running on SMC output to https://<SMC_HOST>/smc/files/smc/tmp
 if(SMC_HOST == "localhost"):
     OUTPUT_FILE = "/lancope/var/smc/tmp/UserReport.csv"
+    # when running local to SMC output to https://<SMC_HOST>/smc/files/smc/tmp
 else:
     OUTPUT_FILE = "./UserReport.csv"
+    # Note:  output file is TAB SEPARATED, can be opened by Microsoft Excel
 
 
 # Set the URL for SMC login
@@ -69,23 +70,22 @@ login_request_data = {
 }
 
 # Initialize the Requests session
+#api_session = requests.Session()
 api_session = requests.Session()
 
-# Perform the POST request to login
-response = api_session.request("POST", url, verify=False, data=login_request_data)
+
+# Perform the POST request to login, or give a helpful (at least amusing) error message.
+try:
+    response = api_session.request("POST", url, verify=False, data=login_request_data)
+except requests.exceptions.RequestException as e:
+    print("\n\n   OH NO!!\n   I have failed you, and the only explanation I have is this:\n")
+    raise SystemExit(e)
+    
 
 # If the login was successful
 if(response.status_code == 200):
 
     try:
-        # Get the list of users from the SMC
-        url = 'https://' + SMC_HOST + '/smc-users/rest/v1/users'
-        response = api_session.request("GET", url, verify=False)
-
-        # Check if the request was successful
-        if (response.status_code != 200):
-            raise RuntimeError("An error has occurred, while fetching users, with the following code: {}".format(response.status_code))
-        
         # Open a file to save output to
         with open(OUTPUT_FILE, mode='w') as output:
 
@@ -94,14 +94,95 @@ if(response.status_code == 200):
             timeStamp = today.strftime('%Y-%m-%d %H:%M:%S')
             print('Data current as of: ' + timeStamp + '\n\n', file=output)
             if not QUIET:
-                print('Data current as of: ' + timeStamp + '\n\n', file=sys.stdout)
+                print('\n\tData current as of: ' + timeStamp + '\n\n', file=sys.stdout)
 
+            # Get the list of data roles from the SMC
+            url = 'https://' + SMC_HOST + '/smc-users/rest/v1/roles/data-roles'
+            response = api_session.request("GET", url, verify=False)
+
+            # Check if the request was successful
+            if (response.status_code != 200):
+                raise RuntimeError("An error has occurred, while fetching roles, with the following code: {}".format(response.status_code))
+
+            # Print all the data roles
+            dRoles = json.loads(response.content)["data"]
+            ###print(dRoles)
+            print("The defined Data Roles are:\n", file=output)
+            if not QUIET:
+                print("The defined Data Roles are:\n", file=sys.stdout)
+            print("roleId\troleName\troleDescription", file=output)
+            for item in dRoles:
+                role = item['id']
+                name = item['name']
+                description = item['description']
+                print(f'{role}\t{name}\t{description}', file=output)
+                if not QUIET:
+                    print(f'{role}\t{name}\t{description}', file=sys.stdout)
+
+            # Get the list of functional roles from the SMC
+            url = 'https://' + SMC_HOST + '/smc-users/rest/v1/roles/functionroles'
+            response = api_session.request("GET", url, verify=False)
+
+            # Check if the request was successful
+            if (response.status_code != 200):
+                raise RuntimeError("An error has occurred, while fetching roles, with the following code: {}".format(response.status_code))
+
+            # Print all the functional roles
+            fRoles = json.loads(response.content)["data"]
+            ###print(fRoles)
+            print("\nThe defined Functional Roles are:\n" +
+                "ROLE\tNAME\tDESCRIPTION", file=output)
+            if not QUIET:
+                print("\nThe defined Functional Roles are:\n" +
+                    "ROLE\tNAME\tDESCRIPTION", file=sys.stdout)
+            for item in fRoles:
+                role = item['id']
+                name = item['name']
+                description = item['description']
+                print(f'{role}\t{name}\t{description}', file=output)
+                if not QUIET:
+                    print(f'{role}\t{name}\t{description}', file=sys.stdout)
+            
+            # Get the list of web function roles from the SMC
+            url = 'https://' + SMC_HOST + '/smc-users/rest/v1/roles/webfunctionroles'
+            response = api_session.request("GET", url, verify=False)
+
+            # Check if the request was successful
+            if (response.status_code != 200):
+                raise RuntimeError("An error has occurred, while fetching roles, with the following code: {}".format(response.status_code))
+
+            # Print all the web functional roles
+            wRoles = json.loads(response.content)["data"]
+            print("\nThe defined Web Functional Roles are:\n" +
+                "ROLE\tNAME\tDESCRIPTION",
+                
+                 file=output)
+            if not QUIET:
+                print("\nThe defined Web Functional Roles are:\n" +
+                    "ROLE\tNAME\tDESCRIPTION", file=sys.stdout)
+            for item in wRoles:
+                role = item['id']
+                name = item['name']
+                description = item['description']
+                print(f'{role}\t{name}\t{description}', file=output)
+                if not QUIET:
+                    print(f'{role}\t{name}\t{description}', file=sys.stdout)
+
+            # Get the list of users from the SMC
+            url = 'https://' + SMC_HOST + '/smc-users/rest/v1/users'
+            response = api_session.request("GET", url, verify=False)
+
+            # Check if the request was successful
+            if (response.status_code != 200):
+                raise RuntimeError("An error has occurred, while fetching users, with the following code: {}".format(response.status_code))
+            
             # Loop through the list of users and print the relevant information.
             data = json.loads(response.content)["data"]
-            print('The User list is:\n', file=output)
+            print('\nThe User list is:\n' +
+                'USERNAME\tFULL NAME\tEMAIL\tIS ENABLED\tDATA ROLE\tFUNCTIONAL ROLES\tWEB FUNCTIONAL ROLES\tIS ADMIN', file=output)
             if not QUIET:
-                print('The User list is:\n', file=sys.stdout)
-            print('user name, full name, email, enabled, data role, function role, web function role, admin', file=output)
+                print('\nThe User list is:\n' +
+                    'USERNAME\tFULL NAME\tEMAIL\t\tIS ENABLED\tDATA ROLE\t\tFUNCTIONAL ROLES\t\t\tWEB FUNCTIONAL ROLES\t\tIS ADMIN', file=sys.stdout)
             for item in data:
                 username = item['userName']
                 if 'fullName' in item:
@@ -113,80 +194,18 @@ if(response.status_code == 200):
                 else:
                     email = "--"
                 enabled = item['enabled']
-                dataId = item['dataRoleId']
-                funcIds = item['functionRoleIds']
-                webFuncIds = item['webFunctionRoleIds']
+                dataRole = next((i.get('name') for i in dRoles if i['id'] == item['dataRoleId']), "--")
+                funcRoles = []
+                for r in item['functionRoleIds']:
+                    funcRoles.append(next((i.get('name') for i in fRoles if i['id'] == r), "--"))
+                webRoles = []
+                for r in item['webFunctionRoleIds']:
+                    webRoles.append(next((i.get('name') for i in wRoles if i['id'] == r), "na"))
                 admin = item['isAdmin']
-                print(f'{username}, {fullname}, {email}, {enabled}, {dataId}, {funcIds}, {webFuncIds}, {admin}', file=output)
+                print(f'{username}\t{fullname}\t{email}\t{enabled}\t{dataRole}\t{funcRoles}\t{webRoles}\t{admin}', file=output)
                 if not QUIET:
-                    print(f'{username}, {fullname}, {email}, {enabled}, {dataId}, {funcIds}, {webFuncIds}, {admin}', file=sys.stdout)
+                    print(f'{username}\t\t{fullname}\t{email}\t\t{enabled}\t\t{dataRole}\t{funcRoles}\t{webRoles}\t{admin}', file=sys.stdout)
             
-            # Get the list of data roles from the SMC
-            url = 'https://' + SMC_HOST + '/smc-users/rest/v1/roles/data-roles'
-            response = api_session.request("GET", url, verify=False)
-
-            # Check if the request was successful
-            if (response.status_code != 200):
-                raise RuntimeError("An error has occurred, while fetching roles, with the following code: {}".format(response.status_code))
-
-            # Print all the data roles
-            data = json.loads(response.content)["data"]
-            print("\nThe defined Data Roles are:\n", file=output)
-            if not QUIET:
-                print("\nThe defined Data Roles are:\n", file=sys.stdout)
-            print("roleId, roleName, roleDescription", file=output)
-            for item in data:
-                role = item['id']
-                name = item['name']
-                description = item['description']
-                print(f'{role}, {name}, {description}', file=output)
-                if not QUIET:
-                    print(f'{role}, {name}, {description}', file=sys.stdout)
-
-            # Get the list of functional roles from the SMC
-            url = 'https://' + SMC_HOST + '/smc-users/rest/v1/roles/functionroles'
-            response = api_session.request("GET", url, verify=False)
-
-            # Check if the request was successful
-            if (response.status_code != 200):
-                raise RuntimeError("An error has occurred, while fetching roles, with the following code: {}".format(response.status_code))
-
-            # Print all the functional roles
-            data = json.loads(response.content)["data"]
-            print("\nThe defined Functional Roles are:\n", file=output)
-            if not QUIET:
-                print("\nThe defined Functional Roles are:\n", file=sys.stdout)
-            print("roleId, roleName, roleDescription", file=output)
-            for item in data:
-                role = item['id']
-                name = item['name']
-                description = item['description']
-                print(f'{role}, {name}, {description}', file=output)
-                if not QUIET:
-                    print(f'{role}, {name}, {description}', file=sys.stdout)
-            
-            # Get the list of web function roles from the SMC
-            url = 'https://' + SMC_HOST + '/smc-users/rest/v1/roles/webfunctionroles'
-            response = api_session.request("GET", url, verify=False)
-
-            # Check if the request was successful
-            if (response.status_code != 200):
-                raise RuntimeError("An error has occurred, while fetching roles, with the following code: {}".format(response.status_code))
-
-            # Print all the web functional roles
-            data = json.loads(response.content)["data"]
-            print("\nThe defined Web Functional Roles are:\n", file=output)
-            if not QUIET:
-                print("\nThe defined Web Functional Roles are:\n", file=sys.stdout)
-            print("roleId, roleName, roleDescription", file=output)
-            for item in data:
-                role = item['id']
-                name = item['name']
-                description = item['description']
-                print(f'{role}, {name}, {description}', file=output)
-                if not QUIET:
-                    print(f'{role}, {name}, {description}', file=sys.stdout)
-
             if not QUIET:
                 print("\n  " + sys.argv[0] + " done.  Check for output in " + OUTPUT_FILE)
         
@@ -198,5 +217,6 @@ if(response.status_code == 200):
 
 # If the login was unsuccessful
 else:
-    print("An error has occurred, while logging in, with the following code {}".format(response.status_code))
-
+    #print("An error has occurred, while logging in, with the following code {}".format(response.status_code))
+    print("\n\n   OH NO!!\n   I cannot log in because when I tried the server said:\n")
+    raise SystemExit(format(response.status_code))
